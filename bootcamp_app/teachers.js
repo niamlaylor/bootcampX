c9f223133f5f84ab990dd5e793fbecff6573d80a
Liam Naylor <liam@liamnaylor.com>
2022-12-08 14:01:51 -0800

    Connect db with js app via pg

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const input = process.argv[2].toUpperCase();
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${input}'
+ORDER BY teacher;
+`)
+.then(res => {
+  console.log('connected');
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
